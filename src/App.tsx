676677e9e99c2dc47f99eecddc3c58bee77645cd
axunion <120321953+axunion@users.noreply.github.com>
2024-09-27 22:53:26 +0900

    Create PagePath components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import PagePath from "@/components/PagePath";
 import Index from "@/pages/Index";
 
 const App = () => {
   return (
     <div className="flex flex-col min-h-screen bg-amber-50 text-amber-900">
       <Header />
+      <PagePath paths={["1", "2"]} />
 
       <main className="flex-grow container mx-auto p-4">
         <Index />
