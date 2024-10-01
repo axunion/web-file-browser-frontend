b9f39f9b55f22b3305bc5e91bc6c6f03001969df
axunion <120321953+axunion@users.noreply.github.com>
2024-10-01 20:49:32 +0900

    Create FileUploadButton components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,19 @@
 import DirectorySvg from "@/assets/DirectorySvg";
+import FileUploadButton from "@/components/FileUploadButton";
 
 const Header = () => {
+  const handleFileChange = (files: File[]): void => {
+    console.log(files);
+  };
+
   return (
-    <header className="px-5 py-3 bg-amber-9 00 text-amber-50 shadow-md">
+    <header className="flex justify-between px-5 py-3 bg-amber-900 text-amber-50 shadow-md">
       <h1 className="text-lg tracking-wider">
         <DirectorySvg className="h-6 mr-2 inline-block align-text-bottom" />
         Web File Browser
       </h1>
+
+      <FileUploadButton onFilesSelected={handleFileChange} />
     </header>
   );
 };
