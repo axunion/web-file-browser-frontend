f11937ad13ee3e80aa8f0d8305bfc3bca7f6f500
axunion <120321953+axunion@users.noreply.github.com>
2024-10-01 20:49:32 +0900

    Create FileUploadButton components

---
+++ src/components/FileUploadButton.tsx
@@ -0,0 +1,30 @@
+import { ChangeEvent } from "react";
+
+interface FileUploadButtonProps {
+  onFilesSelected?: (files: File[]) => void;
+}
+
+const FileUploadButton = ({ onFilesSelected }: FileUploadButtonProps) => {
+  const handleFileChange = (event: ChangeEvent) => {
+    const input = event.target as HTMLInputElement;
+    const files = input.files ? Array.from(input.files) : [];
+
+    if (onFilesSelected) {
+      onFilesSelected(files);
+    }
+  };
+
+  return (
+    <label className="cursor-pointer text-teal-200">
+      <input
+        type="file"
+        className="hidden"
+        multiple
+        onChange={handleFileChange}
+      />
+      <div className="i-mdi-cloud-upload-outline w-6 h-6"></div>
+    </label>
+  );
+};
+
+export default FileUploadButton;
