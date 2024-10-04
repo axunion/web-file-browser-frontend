94d364d7397a86bb65e3b9fc8b53da3b5aa57f69
axunion <120321953+axunion@users.noreply.github.com>
2024-10-04 20:47:29 +0900

    Refine modal functionality and appearance

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-type ModalProps = {
+export type ModalProps = {
   isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
@@ -7,19 +7,24 @@ type ModalProps = {
 const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return null;
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div
-        className="absolute inset-0 bg-black opacity-50"
-        onClick={onClose}
-      ></div>
+  const stopPropagation = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
 
-      <div className="relative w-4/5 max-w-xs max-h-full p-6 rounded bg-amber-50 shadow-lg z-10">
+  return (
+    <div
+      className="fixed inset-0 bg-[#00000080] flex items-center justify-center z-50"
+      onClick={onClose}
+    >
+      <div
+        className="relative w-4/5 max-w-xs max-h-80vh p-5 rounded bg-amber-50 shadow-lg z-10"
+        onClick={stopPropagation}
+      >
         <button
-          className="absolute top-2 right-2 text-amber-900"
+          className="absolute top-3 right-3 text-amber-900"
           onClick={onClose}
         >
-          &times;
+          <div className="i-mdi-close-thick w-6 h-6"></div>
         </button>
 
         {children}
