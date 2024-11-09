7f62befb082ec64283e03a16917294fc1caed1d4
axunion <120321953+axunion@users.noreply.github.com>
2024-11-09 16:07:54 +0900

    Create custom hooks

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,33 +1,18 @@
-import { useState } from "react";
 import FileItem, { type File } from "@/components/FileItem";
 import LoadingSpinner from "@/components/LoadingSpinner";
-
-const data: File[] = [
-  { name: "ドキュメント", type: "folder" },
-  { name: "動画.mp4", type: "video" },
-  { name: "音声.m4a", type: "audio" },
-  { name: "写真.jpg", type: "image" },
-  { name: "メモ.txt", type: "text" },
-];
+import useFetch from "@/hooks/useFetch";
 
 const FileList = () => {
-  const [files, setFiles] = useState<File[]>([]);
-
-  const fetchData = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    setFiles(data);
-  };
-
-  fetchData();
+  const { data } = useFetch<File[]>("");
 
   const gridClasses =
     "grid gap-2 grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10";
 
-  return files.length === 0 ? (
+  return !data || data.length === 0 ? (
     <LoadingSpinner />
   ) : (
     <div className={`fade-in ${gridClasses}`}>
-      {files.map((file, index) => (
+      {data.map((file, index) => (
         <FileItem key={`${file.name}-${index}`} file={file} />
       ))}
     </div>
