fcfe9f20b2f9ac739f4a5f719bf98e51e7f9c659
axunion <120321953+axunion@users.noreply.github.com>
2024-11-09 16:07:54 +0900

    Create custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import FileList from "@/components/FileList";
 import Header from "@/components/Header";
 import PagePath from "@/components/PagePath";
 import ModalProvider from "@/contexts/ModalProvider";
+import useHash from "@/hooks/useHash";
 
 const App = () => {
+  const [hash] = useHash();
+  const paths = hash.slice(1).split("/").filter(Boolean);
+
   return (
     <ModalProvider>
       <div className="flex flex-col min-h-screen bg-amber-50 text-amber-900">
         <Header />
-        <PagePath paths={["1", "2"]} />
+        <PagePath paths={paths} />
 
         <main className="flex-grow container mx-auto p-4">
           <FileList />
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react";
+
+type Params = Record<string, string | number | boolean>;
+type FetchState<T> = {
+  data: T | null;
+  error: Error | null;
+  loading: boolean;
+};
+
+const useFetch = <T>(
+  baseUrl: string,
+  params?: Params,
+  options?: RequestInit
+): FetchState<T> => {
+  const [state, setState] = useState<FetchState<T>>({
+    data: null,
+    error: null,
+    loading: true,
+  });
+
+  useEffect(() => {
+    // const url = new URL(baseUrl);
+
+    // if (params) {
+    //   Object.entries(params).forEach(([key, value]) => {
+    //     url.searchParams.append(key, String(value));
+    //   });
+    // }
+
+    const controller = new AbortController();
+    // const { signal } = controller;
+
+    const fetchData = async () => {
+      try {
+        setState((prev) => ({ ...prev, loading: true }));
+        // const response = await fetch(url.toString(), { ...options, signal });
+
+        // if (!response.ok) {
+        //   throw new Error(
+        //     `HTTP error! status: ${response.status} - ${response.statusText}`
+        //   );
+        // }
+
+        // const json = await response.json();
+        const json = [
+          { name: "ドキュメント", type: "folder" },
+          { name: "動画.mp4", type: "video" },
+          { name: "音声.m4a", type: "audio" },
+          { name: "写真.jpg", type: "image" },
+          { name: "メモ.txt", type: "text" },
+        ] as T;
+        await new Promise((resolve) => setTimeout(resolve, 500));
+
+        setState({
+          data: json,
+          error: null,
+          loading: false,
+        });
+      } catch (error: unknown) {
+        if (error instanceof DOMException && error.name === "AbortError") {
+          console.log("Fetch aborted");
+          return;
+        }
+
+        setState({
+          data: null,
+          error: error instanceof Error ? error : new Error("Unknown error"),
+          loading: false,
+        });
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [baseUrl, options, params]);
+
+  return state;
+};
+
+export default useFetch;
