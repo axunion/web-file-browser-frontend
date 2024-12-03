074e7de5a670be8300c0d88931c15e6685569ad9
axunion <120321953+axunion@users.noreply.github.com>
2024-12-03 23:19:57 +0900

    Update fetch implementation and restructure its positioning

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,23 +1,17 @@
-import type { ApiResponse } from "@/types/api";
+import type { DirectoryItem } from "@/types/api";
 import FileItem from "@/components/FileItem";
-import LoadingSpinner from "@/components/LoadingSpinner";
-import useFetch from "@/hooks/useFetch";
-import useHash from "@/hooks/useHash";
 
-const FileList = () => {
-  const [hash] = useHash();
-  const baseUrl = import.meta.env.VITE_ENDPOINT_LIST;
-  const params = hash ? { path: hash.slice(1) } : undefined;
-  const { response, error, loading } = useFetch<ApiResponse>(baseUrl, params);
+export type FileListProps = {
+  list: DirectoryItem[];
+};
 
+const FileList = ({ list }: FileListProps) => {
   const gridClasses =
     "grid gap-x-2 gap-y-4 grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10";
 
-  return loading || error || !response || !response.list ? (
-    <LoadingSpinner />
-  ) : (
+  return (
     <div className={`fade-in ${gridClasses}`}>
-      {response.list.map((file, index) => (
+      {list.map((file, index) => (
         <div key={`${file.name}-${index}`}>
           <FileItem file={file} />
         </div>
