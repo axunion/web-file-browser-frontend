860a6e9498e8523763ec9fcb98f2426bcc97527e
axunion <120321953+axunion@users.noreply.github.com>
2024-12-13 23:40:17 +0900

    Add SWR for data fetching and implement useFileList hook

---
+++ src/hooks/useFileList.ts
@@ -0,0 +1,20 @@
+import useSWR from "swr";
+import type { ApiResponse } from "@/types/api";
+
+type Fetcher = (...args: [RequestInfo, RequestInit?]) => Promise<ApiResponse>;
+
+const endpoint: string = import.meta.env.VITE_ENDPOINT_LIST;
+const fetcher: Fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const useFileList = (path: string) => {
+  const key = `${endpoint}${path}`;
+  const { data, error, isLoading } = useSWR<ApiResponse, Error>(key, fetcher);
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+};
+
+export default useFileList;
