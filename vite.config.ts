4b5b668a0836a6e717342c282f042b4daccbedea
axunion <120321953+axunion@users.noreply.github.com>
2024-12-25 23:07:57 +0900

    Update configs for framework and formatter changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
+import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
 import UnoCSS from "unocss/vite";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), UnoCSS()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [react(), UnoCSS()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 });
