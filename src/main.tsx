e4a73d02a37dc6c4a3c52e40b6079399751750b7
axunion <120321953+axunion@users.noreply.github.com>
2025-01-09 20:32:29 +0900

    Replace Unocss with Tailwind CSS framework

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "@unocss/reset/tailwind.css";
-import "virtual:uno.css";
 import "@/App.css";
 
 const rootElement = document.getElementById("root");
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react-swc";
-import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), UnoCSS()],
+	plugins: [react()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
