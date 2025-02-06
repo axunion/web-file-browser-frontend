8a8d12dc68a6488ed42d82370b0dc31878f7f5c2
axunion <120321953+axunion@users.noreply.github.com>
2025-02-06 23:03:40 +0900

    Replace spinner with icon for better UX

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,9 +1,10 @@
+import { Icon } from "@iconify/react";
 import { createPortal } from "react-dom";
 
 const LoadingSpinner = () => {
 	return createPortal(
 		<div className="fixed inset-0 bg-[#ffffff80] flex items-center justify-center z-50">
-			<div className="animate-spin rounded-full h-8 w-8 border-t-4 border-amber-900" />
+			<Icon icon="eos-icons:loading" className="h-10 w-10" />
 		</div>,
 		document.body,
 	);
