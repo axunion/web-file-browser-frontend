cf39c21401ea1e6a1e33a8c146f7c0de6fca6f96
axunion <120321953+axunion@users.noreply.github.com>
2025-02-07 23:05:36 +0900

    Refactor FileItem display logic

--- src/components/FileList.tsx
+++ src/components/FileList.tsx
@@ -1,35 +1,38 @@
-import { appendPath } from "@/utils/path";
 import FileItem from "@/components/FileItem";
 import type { DirectoryItem } from "@/types/api";
+import { appendPath, getPath } from "@/utils/path";
 import { Icon } from "@iconify/react";
 
 export type FileListProps = {
 	list: DirectoryItem[];
 };
 
+const endpoint: string = import.meta.env.VITE_ENDPOINT_DATA ?? "";
+
 const FileList = ({ list }: FileListProps) => {
+	const dirPath = `${endpoint}${getPath().path}/`;
 	const gridClasses =
 		"grid gap-x-2 gap-y-4 grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10";
 
 	return (
 		<div className={`fade-in ${gridClasses}`}>
-			{list.map((file, index) => (
-				<div key={`${file.name}-${index}`}>
-					{file.type === "directory" ? (
+			{list.map((item, index) => (
+				<div key={`${item.name}-${index}`}>
+					{item.type === "directory" ? (
 						<button
 							type="button"
-							aria-label={`File type is ${file.type}`}
+							aria-label={`File type is ${item.type}`}
 							className="max-w-full mx-auto flex flex-col items-center justify-center p-2 cursor-pointer"
-							onClick={() => appendPath(file.name)}
+							onClick={() => appendPath(item.name)}
 						>
 							<Icon icon="flat-color-icons:folder" className="w-16 h-16" />
 
-							<div className="line-clamp-2 mt-1 px-2 text-xs text-left break-all">
-								{file.name}
+							<div className="line-clamp-2 px-2 text-xs text-left break-all">
+								{item.name}
 							</div>
 						</button>
 					) : (
-						<FileItem file={file} />
+						<FileItem file={item} dirPath={dirPath} />
 					)}
 				</div>
 			))}
