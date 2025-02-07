9e5883657c6ee725901cad7686180f29c5b949a8
axunion <120321953+axunion@users.noreply.github.com>
2025-02-07 23:05:36 +0900

    Refactor FileItem display logic

--- src/components/FileItem.tsx
+++ src/components/FileItem.tsx
@@ -1,44 +1,45 @@
 import type { DirectoryItem } from "@/types/api";
 import { getFileType } from "@/utils/fileType";
-import { appendPath } from "@/utils/path";
 import { Icon } from "@iconify/react";
 
 export type FileItemProps = {
 	file: DirectoryItem;
+	dirPath: string;
 };
 
 const iconSize = "w-16 h-16";
 const icons = {
-	directory: <Icon icon="flat-color-icons:folder" className={iconSize} />,
-	file: <Icon icon="flat-color-icons:file" className={iconSize} />,
-	video: <Icon icon="flat-color-icons:video-file" className={iconSize} />,
 	audio: <Icon icon="flat-color-icons:audio-file" className={iconSize} />,
-	image: <Icon icon="flat-color-icons:image-file" className={iconSize} />,
 	text: <Icon icon="flat-color-icons:document" className={iconSize} />,
+	file: <Icon icon="flat-color-icons:file" className={iconSize} />,
 };
 
-const FileItem = ({ file }: FileItemProps) => {
+const FileItem = ({ file, dirPath }: FileItemProps) => {
 	const fileType = getFileType(file.name);
+	const src = `${dirPath}/${file.name}`;
 
-	const handleClick = () => {
-		if (file.type === "directory") {
-			appendPath(file.name);
+	const renderContent = () => {
+		switch (fileType) {
+			case "image":
+				return <img src={src} className="h-15 pb-1" alt="" />;
+			case "video":
+				return (
+					<video src={src} className="h-16 aspect-video" controls>
+						<track kind="captions" />
+					</video>
+				);
+			default:
+				return icons[fileType] || icons.file;
 		}
 	};
 
 	return (
-		<button
-			type="button"
-			aria-label={`File type is ${file.type}`}
-			className="max-w-full mx-auto flex flex-col items-center justify-center p-2 cursor-pointer"
-			onClick={handleClick}
-		>
-			{icons[fileType]}
-
+		<span className="max-w-full mx-auto flex flex-col items-center justify-center p-2 cursor-pointer">
+			{renderContent()}
 			<div className="line-clamp-2 mt-1 px-2 text-xs text-left break-all">
 				{file.name}
 			</div>
-		</button>
+		</span>
 	);
 };
 
