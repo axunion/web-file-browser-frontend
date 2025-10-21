36d8274e4f6dfc9c44a619b23b038461d0765c61
axunion <120321953+axunion@users.noreply.github.com>
2025-10-21 23:40:51 +0900

    feat: enhance FileItem and FileList components for improved file display and interaction

--- src/components/FileItem.tsx
+++ src/components/FileItem.tsx
@@ -9,37 +9,48 @@ export type FileItemProps = {
 
 const iconSize = "w-16 h-16";
 const icons = {
+	video: <Icon icon="flat-color-icons:video-file" className={iconSize} />,
+	audio: <Icon icon="flat-color-icons:audio-file" className={iconSize} />,
 	text: <Icon icon="flat-color-icons:document" className={iconSize} />,
 	pdf: <Icon icon="vscode-icons:file-type-pdf2" className={iconSize} />,
 	file: <Icon icon="flat-color-icons:file" className={iconSize} />,
 };
+const nameStyle = "line-clamp-2 p-1 text-xs break-all";
 
 const FileItem = ({ file, dirPath }: FileItemProps) => {
+	if (file.type === "directory") {
+		return (
+			<>
+				<Icon icon="flat-color-icons:folder" className={iconSize} />
+				<div className={nameStyle}>{file.name}</div>
+			</>
+		);
+	}
+
 	const fileType = getFileType(file.name);
 	const src = `${dirPath}${file.name}`;
 
-	switch (fileType) {
-		case "video":
-			return (
-				<video controls src={src} className="h-16 aspect-video" preload="none">
-					<track kind="captions" />
-				</video>
-			);
-		case "audio":
-			return (
-				<audio controls src={src} preload="none">
-					<track kind="captions" />
-				</audio>
-			);
-		case "image":
-			return <img src={src} className="h-15 pb-1" alt="" loading="lazy" />;
-		default:
-			return (
-				<a href={src} target="_blank" rel="noreferrer">
-					{icons[fileType] ?? icons.file}
-				</a>
-			);
+	if (fileType === "image") {
+		return (
+			<>
+				<img src={src} className="h-16" alt="" loading="lazy" />
+				<div className={nameStyle}>{file.name}</div>
+			</>
+		);
 	}
+
+	return (
+		<a
+			href={src}
+			target="_blank"
+			rel="noreferrer"
+			className="flex flex-col items-center"
+			onClick={(e) => e.stopPropagation()}
+		>
+			{icons[fileType] ?? icons.file}
+			<div className={nameStyle}>{file.name}</div>
+		</a>
+	);
 };
 
 export default FileItem;
